1727c153e588e003d09bb02832e57342a0bc9062
3120005395谭圣霖 <1737117947@qq.com>
2022-10-06 00:45:47 +0800

    基本样式

--- 第四次作业/myapp/src/App.jsx
+++ 第四次作业/myapp/src/App.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
-import './CSS/index.less'
-import { Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+import React from "react";
+import "./CSS/index.less";
+import { Radio, Input, Button, Layout } from "antd";
+import {
+  KeyOutlined,
+  StarOutlined,
+  FullscreenExitOutlined,
+} from "@ant-design/icons";
+const { Header } = Layout;
 
 export default function App() {
   return (
-    <div>
     <Layout id="app">
-      <Header>Header</Header>
+      <Header id="header">
+        <div>信息加密</div>
+      </Header>
       <Layout id="contain">
-        <Sider className='sider'>Sider</Sider>
-        <Content className='content'>Content</Content>
+        <div>
+          <span style={{fontWeight:'800', fontSize:'18px'}}>算法选择：</span>
+          <Radio.Group className="select" defaultValue="a" buttonStyle="solid">
+            <Radio.Button value="a">Carsar</Radio.Button>
+            <Radio.Button value="b">Playfair</Radio.Button>
+            <Radio.Button value="c">Hill</Radio.Button>
+          </Radio.Group>
+        </div>
+        <br />
+
+        <Input
+          className="input"
+          size="large "
+          placeholder="密钥"
+          prefix={<KeyOutlined />}
+        />
+        <br />
+
+        <Input
+          size="large "
+          className="input"
+          placeholder="明文"
+          prefix={<StarOutlined />}
+        />
+        <br />
+
+        <Button
+          type="primary"
+          shape="round"
+          icon={<FullscreenExitOutlined />}
+          size="large"
+        >
+          加密
+        </Button>
       </Layout>
-      <Footer>Footer</Footer>
-    </Layout></div>
-  )
+      <div id="result">jieguo</div>
+    </Layout>
+  );
 }
---
+++ 第四次作业/myapp/src/components/MyContent/MyContent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function MyContent() {
+  return (
+    <div>MyContent</div>
+  )
+}
+
