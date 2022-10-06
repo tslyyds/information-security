9453cf6d7885a84c80902d18d3db4284d552260a
3120005395谭圣霖 <1737117947@qq.com>
2022-10-06 15:14:58 +0800

    重新弄,使用原始页面

--- 第四次作业/myapp/src/App.jsx
+++ 第四次作业/myapp/src/App.jsx
@@ -1,56 +1,9 @@
-import React from "react";
-import "./CSS/index.less";
-import { Radio, Input, Button, Layout } from "antd";
-import {
-  KeyOutlined,
-  StarOutlined,
-  FullscreenExitOutlined,
-} from "@ant-design/icons";
-const { Header } = Layout;
+import React from 'react'
 
 export default function App() {
   return (
-    <Layout id="app">
-      <Header id="header">
-        <div>信息加密</div>
-      </Header>
-      <Layout id="contain">
-        <div>
-          <span style={{fontWeight:'800', fontSize:'18px'}}>算法选择：</span>
-          <Radio.Group className="select" defaultValue="a" buttonStyle="solid">
-            <Radio.Button value="a">Carsar</Radio.Button>
-            <Radio.Button value="b">Playfair</Radio.Button>
-            <Radio.Button value="c">Hill</Radio.Button>
-          </Radio.Group>
-        </div>
-        <br />
-
-        <Input
-          className="input"
-          size="large "
-          placeholder="密钥"
-          prefix={<KeyOutlined />}
-        />
-        <br />
-
-        <Input
-          size="large "
-          className="input"
-          placeholder="明文"
-          prefix={<StarOutlined />}
-        />
-        <br />
-
-        <Button
-          type="primary"
-          shape="round"
-          icon={<FullscreenExitOutlined />}
-          size="large"
-        >
-          加密
-        </Button>
-      </Layout>
-      <div id="result">jieguo</div>
-    </Layout>
-  );
+    <div>
+      
+    </div>
+  )
 }
